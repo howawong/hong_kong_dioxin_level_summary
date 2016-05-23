a5e4d09f7b3cbd2a96ab1a4dbef148e18ea039ec
Ho Wa Wong <howa.wong@gmail.com>
2016-05-23 17:04:57 +0000

    Initial Commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,58 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import scraperwiki
+from urlparse import urljoin
+import calendar
+from datetime import datetime
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+class DioxinSpider(scrapy.Spider):
+    name = "dioxin"
+    def start_requests(self):
+        yield scrapy.Request('http://www.aqhi.gov.hk/en/download/air-quality-reports2985.html?showall=&start=3', callback=self.parse_list)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+
+    def parse_list(self, response):
+        table = response.xpath('//div[@class=\'item-page\']/table')
+        rows = table.xpath("tbody/tr")
+        for row in rows[1:]:
+            link = urljoin(response.url, row.xpath(".//a/@href").extract()[0])
+            print "crawling %s" % (link)
+            yield scrapy.Request(link, callback=self.parse_summary)
+
+    def parse_summary(self, response):
+        table = response.xpath('//div[@class=\'item-page\']/table//table')
+        h2    = response.xpath('//div[@class=\'item-page\']/h2/text()').extract()[0].strip()
+        print "[%s]" % h2
+        year = int(h2.split(" ")[-1].strip())
+        rows = table.xpath(".//tr")
+        last_month = 0
+        for row in rows[2:]:
+            cells = row.xpath(".//td")
+            #print len(cells)
+            if len(cells) == 4:
+                month_str = cells[0].xpath(".//span/text()").extract()[0].strip()
+                last_month = list(calendar.month_name).index(month_str)
+                cells = cells[1:]
+            if len(cells) == 3:
+                sampling_date_str = cells[0].xpath(".//span/text()").extract()[0].strip()
+                if sampling_date_str == "Annual Average":
+                    continue
+                sampling_date     = datetime.strptime(sampling_date_str, "%d-%m-%Y")
+                central_str       = cells[1].xpath(".//span/text()").extract()[0].strip()
+                tsuen_wan_str     = cells[2].xpath(".//span/text()").extract()[0].strip()
+                central = None if central_str == "--" else float(central_str)
+                tsuen_wan = None if tsuen_wan_str == "--" else float(tsuen_wan_str)
+                d = {"central": central, "tsuen_wan": tsuen_wan, "sampling_date": sampling_date, 'month':last_month, 'year': year}
+                print d
+                scraperwiki.sqlite.save(unique_keys=["year", "month"], data=d)
+                #print "%d,%s,%s,%s" % (last_month,sampling_date, central_str, tsuen_wan_str)
+
+ 
+
+scraperwiki.sqlite.execute("DROP TABLE IF EXISTS data")
+process = CrawlerProcess()
+process.crawl(DioxinSpider)
+process.start()
+
+ 
+
